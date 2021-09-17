87cf3c1ff01c3ee7c1a961f4341ea5ad466d7375
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-17 20:15:11 +0200

    squelette done

---
+++ app/Views/layout/header.tpl.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <base href="<?= $_SERVER['BASE_URI'] ?>/">
+
+    <title>DevFolio Bootstrap Portfolio Template - Index</title>
+    <meta content="Site portfolio de Jimmy Martin" name="description">
+    <meta content="php portfolio" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- utils CSS Files -->
+    <link href="assets/utils/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/utils/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/utils/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/utils/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <h1 class="logo"><a href="index.html">DevFolio</a></h1>
+
+            <?php require_once __DIR__ . '/../partials/nav.tpl.php' ?>
+
+        </div>
+    </header><!-- End Header -->
+
+    <?php require_once __DIR__ . '/../partials/hero.tpl.php' ?>
+
+    <main id="main">
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../app/Views/layout/header.tpl.php';
+require_once __DIR__ . '/../app/Views/home/home.tpl.php';
+require_once __DIR__ . '/../app/Views/layout/footer.tpl.php';
