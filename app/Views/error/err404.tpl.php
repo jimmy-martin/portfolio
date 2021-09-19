26e0de0dad66afb1f4f3fb30e5b57281752eefa2
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-19 23:46:51 +0200

    error page done

--- app/Views/error/err404.tpl.php
+++ app/Views/error/err404.tpl.php
@@ -1,5 +1,18 @@
-<h1>404 Not Found</h1>
-
-<p>
-    La ressource demandée n'existe pas...
-</p>
+<section id="work" class="portfolio-mf sect-pt4 route sect-mt4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="title-box text-center">
+                    <h3 class="title-a">
+                        404 Not Found
+                    </h3>
+                    <p class="subtitle-a">
+                        La ressource demandée n'existe pas...
+                    </p>
+                    </p>
+                    <div class="line-mf"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
