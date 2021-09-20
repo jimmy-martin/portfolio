8882cd3bbe3ae0f313bf40fec03714c209636b99
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-20 15:08:17 +0200

    favicon update & hero update

--- app/Views/partials/hero.tpl.php
+++ app/Views/partials/hero.tpl.php
@@ -7,7 +7,7 @@
           <!--<p class="display-6 color-d">Hello, world!</p>-->
           <h1 class="hero-title mb-4">Bienvenue,</h1>
           <p class="hero-subtitle">je suis <span class="typed"
-              data-typed-items="Jimmy Martin, à la recherche d'une alternance, à la recherche d'un CDI, à la recherche d'un stage"></span></p>
+              data-typed-items="Jimmy Martin, à la recherche d'une alternance"></span></p>
           <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">En savoir plus&hellip;</a></p>
         </div>
       </div>
